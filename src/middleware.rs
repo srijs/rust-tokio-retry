46af8f289f32ccae7d6a9bcd35881d85191475af
Sam Rijs <srijs@airpost.net>
2017-03-31 23:51:37 +1100

    rename X -> S

--- src/middleware.rs
+++ src/middleware.rs
@@ -6,32 +6,32 @@ use tokio_core::reactor::Handle;
 
 use super::{RetryFuture, RetryError, Action};
 
-pub struct ServiceAction<X: Service> {
-    inner: Arc<X>,
-    request: X::Request
+pub struct ServiceAction<S: Service> {
+    inner: Arc<S>,
+    request: S::Request
 }
 
-impl<X: Service> Action for ServiceAction<X> where X::Request: Clone {
-    type Error = X::Error;
-    type Item = X::Response;
-    type Future = X::Future;
+impl<S: Service> Action for ServiceAction<S> where S::Request: Clone {
+    type Error = S::Error;
+    type Item = S::Response;
+    type Future = S::Future;
 
     fn run(&mut self) -> Self::Future {
         self.inner.call(self.request.clone())
     }
 }
 
-pub type ServiceRetryFuture<I, X> = RetryFuture<I, ServiceAction<X>>;
+pub type ServiceRetryFuture<I, S> = RetryFuture<I, ServiceAction<S>>;
 
 /// Middleware that adds retries to a service via a retry strategy.
-pub struct RetryService<I, X> {
-    inner: Arc<X>,
+pub struct RetryService<I, S> {
+    inner: Arc<S>,
     handle: Handle,
     strategy: I
 }
 
-impl<I: Iterator<Item=Duration>, X> RetryService<I, X> {
-    pub fn new<T: IntoIterator<IntoIter=I, Item=Duration>>(handle: Handle, strategy: T, inner: X) -> RetryService<I, X> {
+impl<I: Iterator<Item=Duration>, S> RetryService<I, S> {
+    pub fn new<T: IntoIterator<IntoIter=I, Item=Duration>>(handle: Handle, strategy: T, inner: S) -> RetryService<I, S> {
         RetryService{
             inner: Arc::new(inner),
             handle: handle,
@@ -40,11 +40,11 @@ impl<I: Iterator<Item=Duration>, X> RetryService<I, X> {
     }
 }
 
-impl<I: Clone + Iterator<Item=Duration>, X: Service> Service for RetryService<I, X> where X::Request: Clone {
-    type Request = X::Request;
-    type Response = X::Response;
-    type Error = RetryError<X::Error>;
-    type Future = ServiceRetryFuture<I, X>;
+impl<I: Clone + Iterator<Item=Duration>, S: Service> Service for RetryService<I, S> where S::Request: Clone {
+    type Request = S::Request;
+    type Response = S::Response;
+    type Error = RetryError<S::Error>;
+    type Future = ServiceRetryFuture<I, S>;
 
     fn call(&self, request: Self::Request) -> Self::Future {
         let action = ServiceAction{
