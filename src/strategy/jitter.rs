7b7919334d5d489e91faee7bd3f23ba08b60783f
Sam Rijs <srijs@airpost.net>
2017-04-02 22:38:17 +1000

    quickcheck tests for jitter

---
+++ src/strategy/jitter.rs
@@ -0,0 +1,40 @@
+use std::time::Duration;
+use rand::{random, Closed01};
+
+fn apply_jitter(duration: Duration, jitter: f64) -> Duration {
+    let secs = (duration.as_secs() as f64) * jitter;
+    let nanos = (duration.subsec_nanos() as f64) * jitter;
+    let millis = (secs * 1000f64) + (nanos / 1000000f64);
+    Duration::from_millis(millis as u64)
+}
+
+pub fn jitter(duration: Duration) -> Duration {
+    let Closed01(jitter) = random();
+    apply_jitter(duration, jitter)
+}
+
+#[test]
+fn apply_jitter_quickcheck() {
+    extern crate quickcheck;
+
+    #[derive(Clone, Debug)]
+    struct ArbitraryJitter(f64);
+
+    impl quickcheck::Arbitrary for ArbitraryJitter {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let Closed01(jitter) = g.gen();
+            ArbitraryJitter(jitter)
+        }
+    }
+
+    fn rounds_correctly(millis: u64, arb_jitter: ArbitraryJitter) -> bool {
+        let ArbitraryJitter(jitter) = arb_jitter;
+
+        let millis_with_jitter = ((millis as f64) * jitter) as u64;
+        let duration_with_jitter = apply_jitter(Duration::from_millis(millis), jitter);
+
+        duration_with_jitter == Duration::from_millis(millis_with_jitter)
+    }
+
+    quickcheck::quickcheck(rounds_correctly as fn(u64, ArbitraryJitter) -> bool)
+}
--- src/strategy/mod.rs
+++ src/strategy/mod.rs
@@ -1,17 +1,9 @@
-use std::time::Duration;
-use rand::{random, Closed01};
-
 mod fixed_interval;
 mod exponential_backoff;
 mod fibonacci_backoff;
+mod jitter;
 
 pub use self::fixed_interval::FixedInterval;
 pub use self::exponential_backoff::ExponentialBackoff;
 pub use self::fibonacci_backoff::FibonacciBackoff;
-
-pub fn jitter(duration: Duration) -> Duration {
-    let Closed01(jitter) = random::<Closed01<f64>>();
-    let secs = ((duration.as_secs() as f64) * jitter).ceil() as u64;
-    let nanos = ((duration.subsec_nanos() as f64) * jitter).ceil() as u32;
-    return Duration::new(secs, nanos);
-}
+pub use self::jitter::jitter;
