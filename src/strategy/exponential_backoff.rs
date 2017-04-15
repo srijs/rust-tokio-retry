52d4052127105e989bc1625173e85a7d48e1849a
Christopher Brickley <brickley@gmail.com>
2017-04-15 19:53:23 -0400

    feat(exponential_backoff): add factor
    A factor can be specified which will be multiplied with the retry delay,
    allowing more control over the produced delay values.

--- src/strategy/exponential_backoff.rs
+++ src/strategy/exponential_backoff.rs
@@ -8,7 +8,8 @@ use std::u64::MAX as U64_MAX;
 #[derive(Debug, Clone)]
 pub struct ExponentialBackoff {
     current: u64,
-    base: u64
+    base: u64,
+    factor: u64,
 }
 
 impl ExponentialBackoff {
@@ -18,7 +19,21 @@ impl ExponentialBackoff {
     /// The resulting duration is calculated by taking the base to the `n`-th power,
     /// where `n` denotes the number of past attempts.
     pub fn from_millis(base: u64) -> ExponentialBackoff {
-        ExponentialBackoff{current: base, base: base}
+        ExponentialBackoff {
+            current: base,
+            base: base,
+            factor: 1u64,
+        }
+    }
+
+    /// A multiplicative factor that will be applied to the retry delay.
+    ///
+    /// For example, using a factor of `1000` will make each delay in units of seconds.
+    ///
+    /// Default factor is `1`.
+    pub fn factor(mut self, factor: u64) -> ExponentialBackoff {
+        self.factor = factor;
+        self
     }
 }
 
@@ -26,7 +41,12 @@ impl Iterator for ExponentialBackoff {
     type Item = Duration;
 
     fn next(&mut self) -> Option<Duration> {
-        let duration = Duration::from_millis(self.current);
+        // set delay duration by applying factor
+        let duration = if let Some(duration) = self.current.checked_mul(self.factor) {
+            Duration::from_millis(duration)
+        } else {
+            Duration::from_millis(U64_MAX)
+        };
 
         if let Some(next) = self.current.checked_mul(self.base) {
             self.current = next;
@@ -34,7 +54,7 @@ impl Iterator for ExponentialBackoff {
             self.current = U64_MAX;
         }
 
-        return Some(duration);
+        Some(duration)
     }
 }
 
@@ -63,4 +83,14 @@ fn saturates_at_maximum_value() {
     assert_eq!(s.next(), Some(Duration::from_millis(U64_MAX - 1)));
     assert_eq!(s.next(), Some(Duration::from_millis(U64_MAX)));
     assert_eq!(s.next(), Some(Duration::from_millis(U64_MAX)));
+}
+
+#[test]
+fn can_use_factor_to_get_seconds() {
+    let factor = 1000;
+    let mut s = ExponentialBackoff::from_millis(2).factor(factor);
+
+    assert_eq!(s.next(), Some(Duration::from_secs(2)));
+    assert_eq!(s.next(), Some(Duration::from_secs(4)));
+    assert_eq!(s.next(), Some(Duration::from_secs(8)));
 }
